cfc0addb82df2d5988f6e6ece3718e1c98d8a447
Vitor Moratz <vitor.morato80@gmail.com>
2023-09-18 10:28:18 -0300

    atualization 18-september

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Button from './Button';
 import styles from '@/styles/Header.module.css';
 
 export default function Header({ aboutMeRef, projectsRef, skillsRef }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className={styles['header-container']}>
+    <header className={`${styles['header-container']} ${isMenuOpen ? styles['menu-open'] : ''}`}>
       <div className={styles['header-logo']}>
         <Image src="/logo.png" alt="minha logo" fill />
       </div>
-      <div className={styles['header-buttons']}>
+      <div className={`${styles['header-buttons']} ${isMenuOpen ? styles['menu-open'] : ''}`}>
         <Button variant="variant" text="About me" targetRef={aboutMeRef} />
         <Button variant="variant" text="Projects" targetRef={projectsRef} />
         <Button variant="variant" text="Skills" targetRef={skillsRef} />
         <Button variant="variant" text="Change Theme" />
       </div>
+      <button className={styles['menu-button']} onClick={toggleMenu}>
+        <span className={styles['menu-icon']}>&#8801;</span>
+      </button>
     </header>
   );
 }
