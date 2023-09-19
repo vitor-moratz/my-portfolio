1d4c658a6a7fbdc645c0d5f314700bc6f3f8e679
Vitor Moratz <vitor.morato80@gmail.com>
2023-09-19 16:36:21 -0300

    faltou 1 heheh

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import styles from '@/styles/Footer.module.css';
+import styles from '../styles/Footer.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Saira } from 'next/font/google';
